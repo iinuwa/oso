8eebb02aa8fa51a3b20cc7ff919c2d66ac5a1a23
Isaiah Inuwa <isaiah.inuwa@gmail.com>
2022-04-10 18:03:58 -0500

    Add authorize methods

--- languages/csharp/oso/src/Oso/Oso.cs
+++ languages/csharp/oso/src/Oso/Oso.cs
@@ -110,4 +110,95 @@ public class Oso : Polar
         }
         throw new ForbiddenException();
     }
+
+    /// <summary>
+    /// Ensure that <paramref name="actor" /> is allowed to send <paramref name="request" /> to the server.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// <para>
+    /// Checks the <c>allow_request</c> rule of a policy.
+    /// </para>
+    /// 
+    /// <para>
+    /// If the request is permitted with an <c>allow_request</c> rule in the policy, then this method
+    /// returns nothing. Otherwise, this method raises a <see cref="ForbiddenException" />.
+    /// </para>
+    /// </remarks>
+    /// 
+    /// <param name="actor">The actor performing the request.</param>
+    /// <param name="request">An object representing the request that was sent by the actor.</param>
+    /// <throws cref="OsoException" />
+    public void AuthorizeRequest(object actor, object request)
+    {
+        bool authorized = QueryRuleOnce("allow_request", actor, request);
+        if (!authorized) throw new ForbiddenException();
+    }
+
+    /// <summary>
+    /// Ensure that <paramref name="actor" /> is allowed to perform <paramref name="action" /> on a given <paramref name="resource" />'s <paramref name="field" />.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// If the action is permitted by an <c>allow_field</c> rule in the policy, then this method returns
+    /// nothing. If the action is not permitted by the policy, this method will raise a
+    /// <see cref="ForbiddenException" />.
+    /// </remarks>
+    /// 
+    /// <param name="actor">The actor performing the request.</param>
+    /// <param name="action">The action the actor is attempting to perform on the field.</param>
+    /// <param name="resource">The resource being accessed.</param>
+    /// <param name="field">The name of the field being accessed.</param>
+    /// <throws cref="OsoException" />
+    public void AuthorizeField(object actor, object action, object resource, object field)
+    {
+        bool authorized = QueryRuleOnce("allow_field", actor, action, resource, field);
+        if (!authorized) throw new ForbiddenException();
+    }
+
+    /// <summary>
+    /// Determine the fields of <paramref name="resource" /> on which <paramref name="actor" /> is allowed to perform <paramref name="action" />.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// Uses <c>allow_field</c> rules in the policy to find all allowed fields.
+    /// </remarks>
+    /// 
+    /// <param name="actor">The actor for whom to collect allowed fields.</param>
+    /// <param name="action">The action being taken on the field.</param>
+    /// <param name="resource">The resource being accessed.</param>
+    /// <param name="allowWildcard">
+    /// Flag to determine behavior if the policy includes a wildcard field.
+    /// <para>
+    /// <example>
+    ///     E.g., given a rule allowing any field:
+    ///     <code>
+    ///     allow_field(_actor, _action, _resource, _field)
+    ///     </code>
+    ///     If <c>true</c>, the method will return <c>["*"]</c>, if <c>false</c>, the method will raise an exception.
+    /// </example>
+    /// </para>
+    /// </param>
+    /// <returns> A set of the unique allowed fields.</returns>
+    /// <throws cref="OsoException" />
+    public HashSet<object> AuthorizedFields(object actor, object action, object resource, bool allowWildcard = false)
+    {
+        return QueryRule("allow_field", actor, action, resource, new Variable("field"))
+            .Results.Select(
+                field =>
+                {
+                    if (field["field"] is not Variable) return field["field"];
+                    else
+                    {
+                        return (allowWildcard)
+                            ? "*"
+                            : throw new OsoException(
+                            "\"The result of authorizedFields contained an \"unconstrained\" field that"
+                                + " could represent any\n"
+                                + " field, but allowWildcard was set to false. To fix,\n"
+                                + " set allowWildcard to true and compare with the \"*\"\n"
+                                + " string.\"");
+                    }
+                }).ToHashSet();
+    }
 }
